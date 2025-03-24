3d529186424c471e536bb0fe701b35c955aa47c5
andifilhohub <andersonfilho.contato@outlook.com>
2025-03-23 23:06:47 -0300

    pg connection

--- app/server.js
+++ app/server.js
@@ -1,25 +1,41 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const { Pool } = require('pg');
 
 const app = express();
 const port = 8000; // Porta em que o servidor irá rodar
+const pool = new Pool({
+    user: 'chatwoot',
+    host: 'localhost', // ou IP do seu banco
+    database: 'financecontroller',
+    password: 'Password1!',
+    port: 5432, // Porta padrão do PostgreSQL
+});
 
 // Middleware para analisar o corpo das requisições em JSON
 app.use(bodyParser.json());
 
 // Rota para receber as notificações
-app.post('/notifications', (req, res) => {
-    const notification = req.body;
+app.post('/notifications', async (req, res) => {
+    const { title, message, notify_id } = req.body;
 
-    // Exibe a notificação recebida no console
-    console.log('Notificação recebida:', notification);
+    try {
 
-    // Aqui você pode fazer o que quiser com as notificações (salvar em banco, etc.)
 
-    // Envia uma resposta ao aplicativo
-    res.status(200).send('Notificação recebida com sucesso!');
+
+        const result = await pool.query(
+            'INSERT INTO notificationsReceived (title, message, notification_id) VALUES ($1, $2, $3) RETURNING *',
+            [title, message, notify_id]
+        );
+        console.log('Notificação salva:', result.rows[0]);
+        res.status(201).send('Notificação salva no banco!');
+
+
+    } catch (err) {
+        console.error('Erro ao salvar notificação:', err);
+        res.status(500).send('Erro interno do servidor');
+    }
 });
-
 // Inicia o servidor
 app.listen(port, () => {
     console.log(`Servidor rodando em http://localhost:${port}`);
